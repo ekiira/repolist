8aa97158f3a5718feefad057fdb19fd5954feb66
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2021-05-31 12:27:14 +0100

    add warning message

--- js/index.js
+++ js/index.js
@@ -2,9 +2,15 @@ localStorage.removeItem("user");
 
 const input = document.querySelector(".search-input");
 const button = document.querySelector(".search-button");
+const message = document.querySelector(".message");
 
 button.addEventListener("click", () => {
-  localStorage.setItem("user", input.value);
-  input.value = "";
-  window.location.href = "user.html";
+  if (input.value) {
+    message.textContent = "";
+    localStorage.setItem("user", input.value);
+    input.value = "";
+    window.location.href = "user.html";
+  } else {
+    message.textContent = "Please enter username";
+  }
 });
